fe755d0f56762e914c580b13f58714e177cf7526
Jonathan Ho <hojonathanho@gmail.com>
2014-02-23 11:42:55 -0800

    Static page controller. Home page is a static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 RootsOfSuccess::Application.routes.draw do
+  get "static_pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
