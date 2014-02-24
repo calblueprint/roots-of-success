a12dce068a3783634aa46166cda0a915316e27e0
Vincent Tian <vincenttian16@gmail.com>
2014-02-23 18:12:34 -0800

    initial copied user model

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessible :first_name, :last_name, :email, :password,
+                  :password_confirmation, :remember_me, :type,
+                  :approved, :profile_attributes
+
+  has_many dependent: :destroy
+  has_one :profile, class_name: 'UserProfile'
+  accepts_nested_attributes_for :profile
+
+  def name
+    return "#{first_name} #{last_name}"
+  end
+
+end
