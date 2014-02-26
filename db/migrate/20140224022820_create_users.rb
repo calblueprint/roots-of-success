3c4983166110bd573ab7789981956252c29ca7e4
Vincent Tian <vincenttian16@gmail.com>
2014-02-25 17:19:56 -0800

    fixed indentation, removed test

--- db/migrate/20140224022820_create_users.rb
+++ db/migrate/20140224022820_create_users.rb
@@ -3,10 +3,10 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-	  t.string :first_name
-	  t.string :last_name
-	  t.string :email
-	  t.string :password
+	    t.string :first_name
+	    t.string :last_name
+	    t.string :email
+	    t.string :password
       t.string :password_confirmation
       t.string :remember_me
       t.string :type
