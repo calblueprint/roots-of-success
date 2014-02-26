1331733025af13853de450f8068a93c372218572
Vincent Tian <vincenttian16@gmail.com>
2014-02-25 17:33:35 -0800

    taking out devise functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :trackable, :validatable
 
   attr_accessible :first_name, :last_name, :email, :password,
                   :password_confirmation, :remember_me, :type,
