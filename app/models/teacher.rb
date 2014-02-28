194cf53bf6355f9ecda3a3d0198fc060161a6768
Vincent Tian <vincenttian16@gmail.com>
2014-02-27 16:37:23 -0800

    added annotations

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  first_name :string(255)
+#  last_name  :string(255)
+#  type       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Teacher < User
 end
