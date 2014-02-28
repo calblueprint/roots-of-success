d2b80e0c5330b7fef88fea9ea8184312b28ed6b2
dwarrier <dwarrier@gmail.com>
2014-02-27 16:40:24 -0800

    add student.rb

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,13 @@ ActiveRecord::Schema.define(version: 20140226061704) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "courses", force: true do |t|
+    t.string   "title"
+    t.string   "course_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
