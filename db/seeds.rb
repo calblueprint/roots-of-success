ccd7d0718cd8dadfd31e50257d2f1f142a1168c9
Vincent Tian <vincenttian16@gmail.com>
2014-03-01 20:54:03 -0800

    seed data with emails

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,21 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 def create_dev_team  
-  User.create(first_name: 'Deepak', last_name: 'Warrier')
-  User.create(first_name: 'Alli', last_name: 'Leong')
-  User.create(first_name: 'Ethan', last_name: 'Chiou')
-  User.create(first_name: 'Jonathan', last_name: 'Ho')
-  User.create(first_name: 'Vincent', last_name: 'Tian')
-  User.create(first_name: 'Sam', last_name: 'Lau')
+  Admin.create(first_name: 'Deepak', last_name: 'Warrier', email: 'deeps@deeps.com')
+  Admin.create(first_name: 'Alli', last_name: 'Leong', email: 'alli@alli.com')
+  Admin.create(first_name: 'Ethan', last_name: 'Chiou', email: 'ethan@ethan.com')
+  Admin.create(first_name: 'Jonathan', last_name: 'Ho', email: 'jon@jon.com')
+  Admin.create(first_name: 'Vincent', last_name: 'Tian', email: 'vin@vin.com')
+  Admin.create(first_name: 'Sam', last_name: 'Lau', email: 'sam@sam.com')
 end
 
 def create_users_1  
-  Admin.create(first_name: 'Admin1', last_name: 'Admin1')
-  Admin.create(first_name: 'Admin2', last_name: 'Admin2')
-  Teacher.create(first_name: 'Teacher1', last_name: 'Teacher1')
-  Teacher.create(first_name: 'Teacher2', last_name: 'Teacher2')
-  Student.create(first_name: 'User1', last_name: 'User1')
-  Student.create(first_name: 'User2', last_name: 'User2')
+  Admin.create(first_name: 'Admin1', last_name: 'Admin1', email: 'test1@test.com')
+  Admin.create(first_name: 'Admin2', last_name: 'Admin2', email: 'test2@test.com')
+  Teacher.create(first_name: 'Teacher1', last_name: 'Teacher1', email: 'test3@test.com')
+  Teacher.create(first_name: 'Teacher2', last_name: 'Teacher2', email: 'test4@test.com')
+  Student.create(first_name: 'User1', last_name: 'User1', email: 'test5@test.com')
+  Student.create(first_name: 'User2', last_name: 'User2', email: 'test6@test.com')
 end
 
 create_users_1
