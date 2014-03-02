b32649b7ad6dab366bdbce383a78bf6cbf4ed764
Sam Lau <samlau95@gmail.com>
2014-03-01 22:08:19 -0800

    Annotate and clean up empty specs

--- app/models/learning_module.rb
+++ app/models/learning_module.rb
@@ -1,2 +1,11 @@
+# == Schema Information
+#
+# Table name: learning_modules
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class LearningModule < ActiveRecord::Base
 end
