e99076e7ec74f615b977ee36ba53c953d22aa244
dwarrier <dwarrier@gmail.com>
2014-03-01 20:07:44 -0800

    annotate for Sam

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: classrooms
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#  teacher_id :integer
+#
+
 class Classroom < ActiveRecord::Base
   belongs_to :teacher
   has_many :students
