dca7a89b247c1ec261f35e0bc51b362ec8ec29a1
Vincent Tian <vincenttian16@gmail.com>
2014-03-01 20:59:56 -0800

    added password and bang for create

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,57 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 def create_dev_team  
-  Admin.create(first_name: 'Deepak', last_name: 'Warrier', email: 'deeps@deeps.com')
-  Admin.create(first_name: 'Alli', last_name: 'Leong', email: 'alli@alli.com')
-  Admin.create(first_name: 'Ethan', last_name: 'Chiou', email: 'ethan@ethan.com')
-  Admin.create(first_name: 'Jonathan', last_name: 'Ho', email: 'jon@jon.com')
-  Admin.create(first_name: 'Vincent', last_name: 'Tian', email: 'vin@vin.com')
-  Admin.create(first_name: 'Sam', last_name: 'Lau', email: 'sam@sam.com')
+  Admin.create! first_name: 'Deepak', 
+                last_name: 'Warrier', 
+                password: 'password',
+                email: 'deeps@deeps.com'
+  Admin.create! first_name: 'Alli', 
+                last_name: 'Leong', 
+                password: 'password',
+                email: 'alli@alli.com'
+  Admin.create! first_name: 'Ethan', 
+                last_name: 'Chiou', 
+                password: 'password',
+                email: 'ethan@ethan.com'
+  Admin.create! first_name: 'Jonathan', 
+                last_name: 'Ho', 
+                password: 'password',
+                email: 'jon@jon.com'
+  Admin.create! first_name: 'Vincent', 
+                last_name: 'Tian', 
+                password: 'password',
+                email: 'vin@vin.com'
+  Admin.create! first_name: 'Sam', 
+                last_name: 'Lau', 
+                password: 'password',
+                email: 'sam@sam.com'
 end
 
 def create_users_1  
-  Admin.create(first_name: 'Admin1', last_name: 'Admin1', email: 'test1@test.com')
-  Admin.create(first_name: 'Admin2', last_name: 'Admin2', email: 'test2@test.com')
-  Teacher.create(first_name: 'Teacher1', last_name: 'Teacher1', email: 'test3@test.com')
-  Teacher.create(first_name: 'Teacher2', last_name: 'Teacher2', email: 'test4@test.com')
-  Student.create(first_name: 'User1', last_name: 'User1', email: 'test5@test.com')
-  Student.create(first_name: 'User2', last_name: 'User2', email: 'test6@test.com')
+  Admin.create! first_name: 'Admin1', 
+                last_name: 'Admin1', 
+                password: 'password',
+                email: 'test1@test.com'
+  Admin.create! first_name: 'Admin2', 
+                last_name: 'Admin2', 
+                password: 'password',
+                email: 'test2@test.com'
+  Teacher.create! first_name: 'Teacher1', 
+                last_name: 'Teacher1', 
+                password: 'password',
+                email: 'test3@test.com'
+  Teacher.create! first_name: 'Teacher2', 
+                last_name: 'Teacher2', 
+                password: 'password',
+                email: 'test4@test.com'
+  Student.create! first_name: 'User1', 
+                last_name: 'User1', 
+                password: 'password',
+                email: 'test5@test.com'
+  Student.create! first_name: 'User2', 
+                last_name: 'User2', 
+                password: 'password',
+                email: 'test6@test.com'
 end
 
 create_users_1
