374f03b7b774f89a405b095abe1a26e201ca4946
Vincent Tian <vincenttian16@gmail.com>
2014-03-01 20:47:17 -0800

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def create_dev_team  
+  User.create(first_name: 'Deepak', last_name: 'Warrier')
+  User.create(first_name: 'Alli', last_name: 'Leong')
+  User.create(first_name: 'Ethan', last_name: 'Chiou')
+  User.create(first_name: 'Jonathan', last_name: 'Ho')
+  User.create(first_name: 'Vincent', last_name: 'Tian')
+  User.create(first_name: 'Sam', last_name: 'Lau')
+end
+
+def create_users_1  
+  Admin.create(first_name: 'Admin1', last_name: 'Admin1')
+  Admin.create(first_name: 'Admin2', last_name: 'Admin2')
+  Teacher.create(first_name: 'Teacher1', last_name: 'Teacher1')
+  Teacher.create(first_name: 'Teacher2', last_name: 'Teacher2')
+  Student.create(first_name: 'User1', last_name: 'User1')
+  Student.create(first_name: 'User2', last_name: 'User2')
+end
+
+create_users_1
