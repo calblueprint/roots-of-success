c4eefa4d4be7a9cbbcab19822c8e28c4b76d0884
dwarrier <dwarrier@gmail.com>
2014-03-02 22:33:29 -0800

    take out form error html and add redirect to admin_dashboard_index

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -8,7 +8,7 @@ class AdminDashboardController < ApplicationController
     @teacher = Teacher.new teacher_params
     @teacher.password = SecureRandom.hex 10
     if @teacher.save
-      render 'new_teacher' #should redirect to @teacher once teacher url is created
+      redirect_to admin_dashboard_index_path
     else
       render 'new_teacher'
     end
