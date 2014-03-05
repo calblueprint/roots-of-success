10b118831e4a40af16ac10e602fb2a58476d9280
Vincent Tian <vincenttian16@gmail.com>
2014-03-05 00:53:15 -0800

    no more dream team :/

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,33 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-def create_dev_team  
-  Admin.create! first_name: 'Deepak', 
-                last_name: 'Warrier', 
-                password: 'password',
-                email: 'deeps@deeps.com'
-  Admin.create! first_name: 'Alli', 
-                last_name: 'Leong', 
-                password: 'password',
-                email: 'alli@alli.com'
-  Admin.create! first_name: 'Ethan', 
-                last_name: 'Chiou', 
-                password: 'password',
-                email: 'ethan@ethan.com'
-  Admin.create! first_name: 'Jonathan', 
-                last_name: 'Ho', 
-                password: 'password',
-                email: 'jon@jon.com'
-  Admin.create! first_name: 'Vincent', 
-                last_name: 'Tian', 
-                password: 'password',
-                email: 'vin@vin.com'
-  Admin.create! first_name: 'Sam', 
-                last_name: 'Lau', 
-                password: 'password',
-                email: 'sam@sam.com'
-end
-
 def create_users
   Admin.create! first_name: 'Admin1', 
                 last_name: 'Admin1', 
@@ -43,21 +16,21 @@ def create_users
                 password: 'password',
                 email: 'test2@test.com'
   Teacher.create! first_name: 'Teacher1', 
-                  last_name: 'Teacher1', 
-                  password: 'password',
-                  email: 'test3@test.com'
+                last_name: 'Teacher1', 
+                password: 'password',
+                email: 'test3@test.com'
   Teacher.create! first_name: 'Teacher2', 
-                  last_name: 'Teacher2', 
-                  password: 'password',
-                  email: 'test4@test.com'
+                last_name: 'Teacher2', 
+                password: 'password',
+                email: 'test4@test.com'
   Student.create! first_name: 'User1', 
-                  last_name: 'User1', 
-                  password: 'password',
-                  email: 'test5@test.com'
+                last_name: 'User1', 
+                password: 'password',
+                email: 'test5@test.com'
   Student.create! first_name: 'User2', 
-                  last_name: 'User2', 
-                  password: 'password',
-                  email: 'test6@test.com'
+                last_name: 'User2', 
+                password: 'password',
+                email: 'test6@test.com'
 end
 
 def create_classroom
@@ -72,7 +45,7 @@ def create_classroom
                         last_name: "Student",
                         password: 'password',
                         email: "Student#{n}@gmail.com"
-    student_list.append(s)
+    student_list << s
   end
   c.students = student_list
 end
