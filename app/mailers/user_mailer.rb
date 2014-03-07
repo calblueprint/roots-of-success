a3868c1ac98bb318280a4d97d7e0e7e964308afb
Vincent Tian <vincenttian16@gmail.com>
2014-03-06 20:42:37 -0800

    action mailer implementation

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to Roots of Success!')
+  end
+
+end
