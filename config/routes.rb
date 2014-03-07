9b0ed0a411dfa3068043e403c68a3fe0d69b6244
Ethan Chiou <ethanchiou@berkeley.edu>
2014-03-06 21:36:19 -0800

    Install simple form with foundation, make homepage login page, and style homepage.

--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,9 @@ RootsOfSuccess::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  
-
-
-
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
