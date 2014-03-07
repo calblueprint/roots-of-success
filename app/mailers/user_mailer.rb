6421407577df20bc9fa90970165154ab3784014b
Vincent Tian <vincenttian16@gmail.com>
2014-03-06 21:35:11 -0800

    refactoring code

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,6 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-    # @url  = link_to "Home", root_path
     mail(to: @user.email, subject: 'Welcome to Roots of Success!')
   end
 
