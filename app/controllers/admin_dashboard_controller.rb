b8336ea751f758e63c2d367500b2d940bc05a421
dwarrier <dwarrier@gmail.com>
2014-03-12 20:40:06 -0700

    take out UserMailer

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -9,8 +9,8 @@ class AdminDashboardController < ApplicationController
 
   def create_teacher
     addrs = teacher_params[:emails]
-    @addr_list = addrs.gsub(/\r?\n|\s+/,'').split(',')
-    @bad_addrs = @addr_list.select{ |e| (@t = Teacher.create(email: e, password: (SecureRandom.hex 10))).new_record? || !UserMailer.welcome_email(@t).deliver }
+    addr_list = addrs.strip.split(/,\s*/)
+    @bad_addrs = addr_list.select{ |e| (Teacher.create email: e, password: (SecureRandom.hex 10)).new_record? } 
     if @bad_addrs.empty?
       flash[:success] = "Created all teachers!"
       redirect_to admin_dashboard_index_path
