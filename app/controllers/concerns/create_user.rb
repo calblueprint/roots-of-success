8e0e8e3aae080aa52e3a7668bc6597de981c564a
Vincent Tian <vincenttian16@gmail.com>
2014-05-04 19:01:42 -0700

    user gets reset password email before confirmation email

--- app/controllers/concerns/create_user.rb
+++ app/controllers/concerns/create_user.rb
@@ -2,9 +2,8 @@ class CreateUser
 
   def self.execute(klass, params)
     pass = params[:password]
-    if (u = klass.create(params)).new_record?
-      UserMailer.welcome_email u, pass
-    end
-    return u.new_record?
+    u = klass.create(params)
+    u.send_reset_password_instructions if !u.new_record?
+    u.new_record?
   end
 end 
