509ada79bd587a9c64f9d0b18c0a572ef63cb9ea
Vincent Tian <vincenttian16@gmail.com>
2014-05-04 19:06:17 -0700

    houndci fixes

--- app/controllers/concerns/create_user.rb
+++ app/controllers/concerns/create_user.rb
@@ -3,7 +3,7 @@ class CreateUser
   def self.execute(klass, params)
     pass = params[:password]
     u = klass.create(params)
-    u.send_reset_password_instructions if !u.new_record?
+    u.send_reset_password_instructions unless u.new_record?
     u.new_record?
   end
 end 
