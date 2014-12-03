6536902316c12088a30356d1f1c55c2656017f69
Sam Lau <samlau95@gmail.com>
2014-12-03 00:29:00 -0800

    Remove production GA config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,9 +78,6 @@ RootsOfSuccess::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # Google Analytics tracker code
-  GA.tracker = "UA-50769810-1"
-
   # Set right mailer url
   config.action_mailer.default_url_options = { :host => 'ros-production.herokuapp.com' }
 
