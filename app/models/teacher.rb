ae08ba8733516bbfd96be38480f1c7ebb2bdcd51
Sam Lau <samlau95@gmail.com>
2014-12-16 15:33:12 -0800

    Devise for teachers

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,12 +2,21 @@
 #
 # Table name: teachers
 #
-#  id         :integer          not null, primary key
-#  created_at :datetime
-#  updated_at :datetime
-#  first_name :string(255)
-#  last_name  :string(255)
+#  id                     :integer          not null, primary key
+#  created_at             :datetime
+#  updated_at             :datetime
+#  first_name             :string(255)
+#  last_name              :string(255)
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
 #
 
 class Teacher < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :recoverable,
+         :rememberable, :validatable
 end
