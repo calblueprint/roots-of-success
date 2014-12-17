c2cfb4ca43b15a4b501c0fe6150ca4f9387d6207
Sam Lau <samlau95@gmail.com>
2014-12-17 14:33:22 -0800

    Header component

---
+++ app/assets/javascripts/components/header.js.jsx
@@ -0,0 +1,34 @@
+var Header = React.createClass({
+  // TODO: Make this more declarative
+  slideToggleList: function() {
+    $('#js-navigation-menu').slideToggle(function(){
+      if ($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  },
+
+  render: function() {
+    return (
+      <header className="navigation">
+        <div className="navigation-wrapper">
+          <a href="javascript:void(0)" className="logo">
+            <img src="/assets/RoSLogo.svg" alt="Logo Image"/>
+          </a>
+          <a href="javascript:void(0)" className="navigation-menu-button" id="js-mobile-menu"
+             onClick={this.slideToggleList}>MENU</a>
+          <div className="nav">
+            <ul id="js-navigation-menu" className="navigation-menu">
+              <li className="nav-link"><a href="javascript:void(0)">Products</a></li>
+              <li className="nav-link"><a href="javascript:void(0)">About Us</a></li>
+              <li className="nav-link"><a href="javascript:void(0)">Contact</a></li>
+            </ul>
+          </div>
+          <div className="navigation-tools">
+            <a href="javascript:void(0)" className="sign-up">Sign Up</a>
+          </div>
+        </div>
+      </header>
+    );
+  }
+});
