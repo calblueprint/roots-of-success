f361eb8172cf321b272ef6c0c94ef806f248a7a6
Sam Lau <samlau95@gmail.com>
2014-12-19 20:04:50 -0800

    Remove old router stuff

--- app/assets/javascripts/components/teacher_dashboard.js.jsx
+++ app/assets/javascripts/components/teacher_dashboard.js.jsx
@@ -1,16 +1,10 @@
-var Locations = ReactRouter.Locations;
-var Location = ReactRouter.Location;
-
 var TeacherDashboard = React.createClass({
   render: function() {
     return (
       <div>
         <Header/>
         <div className="spacer"></div>
-        <Locations ref="router">
-          <Location path="/teachers/dashboard" handler={TeacherEdit} />
-          <Location path="/teachers/:id/edit" handler={TeacherEdit} />
-        </Locations>
+        <TeacherEdit/>
       </div>
     );
   }
