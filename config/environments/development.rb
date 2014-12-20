1f177e102f27330e690415b1624a39a5129b5080
Sam Lau <samlau95@gmail.com>
2014-12-19 18:48:52 -0800

    Config react for dev/prod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,7 @@ Rails.application.configure do
   config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  # Use dev react version
+  config.react.variant = :development
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'please-change-me.com' }
+
+  # Use production version of react
+  config.react.variant = :production
 end
