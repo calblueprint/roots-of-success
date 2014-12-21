24893b7ff119273c0317f4733a1347016be0c1da
Sam Lau <samlau95@gmail.com>
2014-12-21 13:42:10 -0800

    Use overridden sessions controller in routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   devise_scope :teacher do
     root to: "devise/sessions#new"
   end
-  devise_for :teachers
+  devise_for :teachers, controllers: { sessions: "teachers/sessions" }
 
   namespace :teachers, as: :teacher do
     resource :dashboard, only: [:show], controller: "dashboard"
