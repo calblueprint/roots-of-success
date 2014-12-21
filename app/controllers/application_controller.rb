d0014569dea0a05d50f5369d4cec342e1728d093
Sam Lau <samlau95@gmail.com>
2014-12-20 16:51:25 -0800

    Simple token auth setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # Prevent CSRF attacks by returning a null session
+  protect_from_forgery with: :null_session, if: proc { |c| c.request.format == 'application/json' }
+
+  acts_as_token_authentication_handler_for Teacher
 
   include DeviseSettings
   include AuthorizationHelpers
