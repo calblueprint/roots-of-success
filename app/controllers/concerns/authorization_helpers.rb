9ae5283e1e682523ae0190583bfadd63f6034fe2
Sam Lau <samlau95@gmail.com>
2014-12-26 14:33:19 -0800

    Cancancan responses to unauthorization json req

--- app/controllers/concerns/authorization_helpers.rb
+++ app/controllers/concerns/authorization_helpers.rb
@@ -3,7 +3,10 @@ module AuthorizationHelpers
 
   included do
     rescue_from CanCan::AccessDenied do |exception|
-      redirect_to root_url, alert: exception.message
+      respond_to do |format|
+        format.html { redirect_to root_url, alert: exception.message }
+        format.json { render json: { access_denied: true }, status: :forbidden }
+      end
     end
   end
 end
