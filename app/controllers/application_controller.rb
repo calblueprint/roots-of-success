a75679ab36e0272cfc46c2fbf8672f31c39c11d3
Sam Lau <samlau95@gmail.com>
2014-12-26 14:54:26 -0800

    Fix csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by returning a null session
-  protect_from_forgery with: :null_session, if: proc { |c| c.request.format == 'application/json' }
-
-  acts_as_token_authentication_handler_for Teacher
+  protect_from_forgery with: :exception, unless: proc { |c| c.request.format == "application/json" }
 
   include DeviseSettings
   include AuthorizationHelpers
