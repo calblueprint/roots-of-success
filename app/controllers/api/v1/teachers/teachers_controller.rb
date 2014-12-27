b2ce48bd993e95ce99638032fd37670369dd6c1b
Sam Lau <samlau95@gmail.com>
2014-12-26 17:48:58 -0800

    Teacher API/auth

--- app/controllers/api/v1/teachers/teachers_controller.rb
+++ app/controllers/api/v1/teachers/teachers_controller.rb
@@ -4,6 +4,10 @@ module Api::V1
       load_and_authorize_resource # loads @teacher
       respond_to :json
 
+      def edit
+        respond_with @teacher
+      end
+
       def update
         render json: { hello: :world }
       end
