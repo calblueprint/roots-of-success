d9a744e508a140fe7a6087ebdf5503e701dfb07a
Sam Lau <samlau95@gmail.com>
2014-12-27 17:44:33 -0800

    Add updatable attributes to serializer

--- app/serializers/teacher_serializer.rb
+++ app/serializers/teacher_serializer.rb
@@ -1,5 +1,9 @@
 class TeacherSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :phone,
-             :position, :location, :curriculum_version_id
   root false
+  attributes :id, :first_name, :last_name, :email, :phone,
+             :position, :location, :curriculum_version_id, :updatable_attrs
+
+  def updatable_attrs
+    [:first_name, :last_name, :email, :phone, :position, :location, :curriculum_version_id]
+  end
 end
