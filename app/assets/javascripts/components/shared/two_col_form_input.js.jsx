531e086777fe69a80217f17ea93ab5c3f0f721d3
Sam Lau <samlau95@gmail.com>
2014-12-27 17:44:18 -0800

    Two col form input comp

---
+++ app/assets/javascripts/components/shared/two_col_form_input.js.jsx
@@ -0,0 +1,14 @@
+var TwoColInput = React.createClass({
+  render: function() {
+    return (
+      <div className="row">
+        <div className="medium-6 columns two-col-form-label">
+          <h4>{this.props.label}</h4>
+        </div>
+        <div className="medium-6 columns">
+          <input type="text" value={this.props.value} onChange={this.props.setValue}/>
+        </div>
+      </div>
+    );
+  }
+});
