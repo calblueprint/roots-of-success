54bd060171f7dc3c14f19e8588329bbd1507695b
Sam Lau <samlau95@gmail.com>
2014-12-27 16:05:39 -0800

    Move router dependencies

--- app/assets/javascripts/components/routers/teacher_router.js.jsx
+++ app/assets/javascripts/components/routers/teacher_router.js.jsx
@@ -1,10 +1,3 @@
-var Router = ReactRouter;
-var Route = Router.Route;
-var NotFoundRoute = Router.NotFoundRoute;
-var DefaultRoute = Router.DefaultRoute;
-var Link = Router.Link;
-var RouteHandler = Router.RouteHandler;
-
 var routes = (
   <Route name="teacher_dashboard" path="/" handler={TeacherDashboard}>
     <Route name="teacher_edit" path="/teachers/:id/edit" handler={TeacherEdit}/>
---
+++ app/assets/javascripts/components/shared/dependencies.js
@@ -0,0 +1,6 @@
+var Router = ReactRouter;
+var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
+var DefaultRoute = Router.DefaultRoute;
+var Link = Router.Link;
+var RouteHandler = Router.RouteHandler;
