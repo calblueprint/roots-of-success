6ed6a3e54a3e1fa9f4f637038477c9e688a6d823
Sam Lau <samlau95@gmail.com>
2014-12-27 17:16:39 -0800

    Finish form and add update actions

--- app/assets/javascripts/components/teachers/teacher_edit.js.jsx
+++ app/assets/javascripts/components/teachers/teacher_edit.js.jsx
@@ -9,19 +9,42 @@ var TeacherEdit = React.createClass({
   },
 
   getInitialState: function getInitialState() {
-    return { teacher: {} };
+    return {};
   },
   componentDidMount: function componentDidMount() {
     $.ajax({
       url: this.editUrl(),
       dataType: "json",
-      success: function ajaxSuccess(teacherData) {
+      success: function editSuccess(teacherData) {
         this.setState(teacherData);
       }.bind(this),
-      error: function ajaxError(xhr, status, err) {
-        console.error(editUrl, status, err.toString());
+      error: function editError(xhr, status, err) {
+        console.error(this.editUrl(), status, err.toString());
+        toastr.error("There was an error connecting to the server. Please refresh and try again.");
       }.bind(this)
     });
+  },
+  updateAttributes: function updateAttributes() {
+    $.ajax({
+      url: this.updateUrl(),
+      type: "PUT",
+      dataType: "json",
+      success: function updateSuccess(response) {
+        console.log(response);
+      }.bind(this),
+      error: function updateError(xhr, status, err) {
+        console.error(this.updateUrl(), status, err.toString());
+        toastr.error("There was an error connecting to the server. Please refresh and try again.");
+      }.bind(this)
+    })
+  },
+
+  createValueSetter: function createValueSetter(value) {
+    return function(event) {
+      var newState = {};
+      newState[value] = event.target.value;
+      this.setState(newState);
+    }.bind(this);
   },
   render: function() {
     return (
@@ -31,14 +54,21 @@ var TeacherEdit = React.createClass({
             <h2>Tell us about yourself.</h2>
             <pre className="left">{JSON.stringify(this.state, undefined, 2)}</pre>
             <div className="spacer"></div>
-            <div className="row">
-              <div className="medium-6 columns two-col-form-label">
-                <h4>My first name is:</h4>
-              </div>
-              <div className="medium-6 columns">
-                <input type="text" value={this.state.teacher.first_name}/>
-              </div>
-            </div>
+            <TwoColInput label="My first name" value={this.state.first_name}
+              setValue={this.createValueSetter("first_name")}/>
+            <TwoColInput label="My last name" value={this.state.last_name}
+              setValue={this.createValueSetter("last_name")}/>
+            <TwoColInput label="My email" value={this.state.email}
+              setValue={this.createValueSetter("email")}/>
+            <TwoColInput label="My phone number" value={this.state.phone}
+              setValue={this.createValueSetter("phone")}/>
+            <TwoColInput label="My position" value={this.state.position}
+              setValue={this.createValueSetter("position")}/>
+            <TwoColInput label="My location" value={this.state.location}
+              setValue={this.createValueSetter("location")}/>
+            <a href="javascript:void(0)" onClick={this.updateAttributes} className="button">
+              Update My Info!
+            </a>
           </Card>
         </div>
       </div>
--- app/serializers/teacher_serializer.rb
+++ app/serializers/teacher_serializer.rb
@@ -1,4 +1,5 @@
 class TeacherSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :email, :phone,
              :position, :location, :curriculum_version_id
+  root false
 end
