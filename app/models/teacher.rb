2f2d79dafb059b8757c8c275002e9a488eb7d42a
Sam Lau <samlau95@gmail.com>
2014-12-29 11:37:35 -0800

    Teacher has many classrooms

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -22,4 +22,6 @@ class Teacher < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  has_many :classrooms
 end
--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -22,4 +22,6 @@ require 'rails_helper'
 RSpec.describe Teacher, type: :model do
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
+
+  it { should have_many :classrooms }
 end
