bdb70d1e8bf7245d239b435156a67aa6bc1a5e40
Sam Lau <samlau95@gmail.com>
2014-12-30 13:04:24 -0800

    Classroom scopes

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -8,13 +8,17 @@
 #  name       :string(255)
 #  teacher_id :integer
 #  program_id :integer
+#  active     :boolean          default(TRUE)
 #
 
 class Classroom < ActiveRecord::Base
+  belongs_to :teacher
+  belongs_to :program
+
   validates :name, presence: true
   validates :teacher_id, presence: true
   validates :program_id, presence: true
 
-  belongs_to :teacher
-  belongs_to :program
+  scope :active, -> { where active: true }
+  scope :inactive, -> { where active: false }
 end
