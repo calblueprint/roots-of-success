9327c66577b77cee3db6b8baaf8b3da075748d1f
Sam Lau <samlau95@gmail.com>
2014-12-30 16:08:48 -0800

    Add description to classrooms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141230203919) do
+ActiveRecord::Schema.define(version: 20141231000455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,8 @@ ActiveRecord::Schema.define(version: 20141230203919) do
     t.string   "name"
     t.integer  "teacher_id"
     t.integer  "program_id"
-    t.boolean  "active",     default: true
+    t.boolean  "active",      default: true
+    t.text     "description"
   end
 
   add_index "classrooms", ["program_id"], name: "index_classrooms_on_program_id", using: :btree
--- spec/factories/classrooms.rb
+++ spec/factories/classrooms.rb
@@ -2,13 +2,14 @@
 #
 # Table name: classrooms
 #
-#  id         :integer          not null, primary key
-#  created_at :datetime
-#  updated_at :datetime
-#  name       :string(255)
-#  teacher_id :integer
-#  program_id :integer
-#  active     :boolean          default(TRUE)
+#  id          :integer          not null, primary key
+#  created_at  :datetime
+#  updated_at  :datetime
+#  name        :string(255)
+#  teacher_id  :integer
+#  program_id  :integer
+#  active      :boolean          default(TRUE)
+#  description :text
 #
 
 FactoryGirl.define do
