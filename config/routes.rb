e2df6beaf9f815fbd6b59886c3f5f547d07f9c9f
Sam Lau <samlau95@gmail.com>
2015-01-01 16:58:35 -0800

    Remove API

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     scope module: :v1 do
-      scope module: :teachers do
-        resources :teachers, only: [:edit, :update] do
-          resources :classrooms, only: [:index, :show, :create, :update, :destroy] do
-            collection { get :inactive }
-          end
-        end
-      end
+      # Future API routes here
     end
   end
 
@@ -17,7 +11,7 @@ Rails.application.routes.draw do
   devise_for :teachers
 
   namespace :teachers, as: :teacher do
-    resource :dashboard, only: [:show], controller: "dashboard"
+    resource :dashboard, only: :show, controller: "dashboard"
   end
 
   scope module: :teachers do
