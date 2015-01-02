8e3748ddf174f6218d4b616d9a4ab21be3740955
Sam Lau <samlau95@gmail.com>
2015-01-01 16:56:37 -0800

    Better auth error handling

--- app/controllers/concerns/authorization_helpers.rb
+++ app/controllers/concerns/authorization_helpers.rb
@@ -4,9 +4,19 @@ module AuthorizationHelpers
   included do
     rescue_from CanCan::AccessDenied do |exception|
       respond_to do |format|
-        format.html { redirect_to root_url, alert: exception.message }
+        format.html { redirect_to path_after_access_denied(current_user), alert: exception.message }
         format.json { render json: { access_denied: true }, status: :forbidden }
       end
+    end
+  end
+
+  private
+
+  def path_after_access_denied(user)
+    if user
+      after_sign_in_path_for(user)
+    else
+      root_path
     end
   end
 end
