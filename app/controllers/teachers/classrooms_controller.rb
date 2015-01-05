ce02e025fbcbfc5066f18b475b3e99af8d7a46d1
Sam Lau <samlau95@gmail.com>
2015-01-04 18:07:00 -0800

    New classroom form

--- app/controllers/teachers/classrooms_controller.rb
+++ app/controllers/teachers/classrooms_controller.rb
@@ -1,11 +1,20 @@
 module Teachers
   class ClassroomsController < BaseController
+    UPDATABLE_ATTRS = [:name, :description, :program_id]
     load_and_authorize_resource # loads @classroom
 
     def new
+      @teacher = Teacher.find params[:teacher_id]
+      @classroom = @teacher.classrooms.build
     end
 
     def show
+    end
+
+    private
+
+    def classroom_params
+      params.require(:classroom).permit(UPDATABLE_ATTRS)
     end
   end
 end
