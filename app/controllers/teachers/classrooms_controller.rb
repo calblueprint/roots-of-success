382082e417519f96e49ff2ea477a347788dad3a4
Sam Lau <samlau95@gmail.com>
2015-01-04 18:44:30 -0800

    Stub methods and load @teacher and @program_collection

--- app/controllers/teachers/classrooms_controller.rb
+++ app/controllers/teachers/classrooms_controller.rb
@@ -3,18 +3,40 @@ module Teachers
     UPDATABLE_ATTRS = [:name, :description, :program_id]
     load_and_authorize_resource # loads @classroom
 
+    before_filter :set_teacher
+    before_filter :set_program_collection, only: [:new, :edit]
+
     def new
-      @teacher = Teacher.find params[:teacher_id]
       @classroom = @teacher.classrooms.build
     end
 
+    def create
+    end
+
     def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
     end
 
     private
 
     def classroom_params
       params.require(:classroom).permit(UPDATABLE_ATTRS)
+    end
+
+    def set_teacher
+      @teacher = Teacher.find params[:teacher_id]
+    end
+
+    def set_program_collection
+      @program_collection = Program.name_id_pairs
     end
   end
 end
