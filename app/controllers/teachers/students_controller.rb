fcf8fc480edaa258a0c6c3eb702bb353b05ceb7d
Sam Lau <samlau95@gmail.com>
2015-01-14 17:33:57 -0800

    Emails form and validations

--- app/controllers/teachers/students_controller.rb
+++ app/controllers/teachers/students_controller.rb
@@ -2,13 +2,22 @@ module Teachers
   class StudentsController < BaseController
     load_and_authorize_resource # loads @student{s}
 
-    before_filter :set_classroom, only: [:index]
+    before_filter :set_classroom, only: [:new, :create, :index]
 
     # new and create actions are modified to create many students at a time.
     def new
     end
 
     def create
+      email_validator = ValidateEmails.new(params[:students][:emails])
+      CreateStudents.execute email_validator.valid_emails
+      if email_validator.all_valid?
+        redirect_to @classroom, flash: { success: "Invited students!" }
+      else
+        @invalid_emails = email_validator.invalid_emails
+        @value = @invalid_emails.join "\n"
+        render "new"
+      end
     end
 
     private
