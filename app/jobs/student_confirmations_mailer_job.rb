c7ca313c4ced4b908dc2270f8c1a48dc5ceaaf33
Sam Lau <samlau95@gmail.com>
2015-01-14 19:48:33 -0800

    Make sure connection is returned to db pool

--- app/jobs/student_confirmations_mailer_job.rb
+++ app/jobs/student_confirmations_mailer_job.rb
@@ -2,11 +2,13 @@ class StudentConfirmationsMailerJob
   include SuckerPunch::Job
 
   def perform(students, options = {})
-    classroom = options[:classroom]
-    fail Exceptions::MissingClassroom unless classroom
+    ActiveRecord::Base.connection_pool.with_connection do
+      classroom = options[:classroom]
+      fail Exceptions::MissingClassroom unless classroom
 
-    students.each do |student|
-      StudentConfirmationMailer.confirmation_email(student, classroom).deliver
+      students.each do |student|
+        StudentConfirmationMailer.confirmation_email(student, classroom).deliver
+      end
     end
   end
 end
