917631a498fa2bb8d0c4adf64529899105cfaa70
Sam Lau <samlau95@gmail.com>
2015-01-16 22:34:52 -0800

    Basic datatable spec

--- spec/features/teachers/students_datatable_spec.rb
+++ spec/features/teachers/students_datatable_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "The students datatable", js: true do
+RSpec.describe "The students datatable" do
   let(:teacher) { create :teacher }
   let(:classroom) { create :classroom, teacher: teacher }
   let!(:students) { create_pair :student, classroom: classroom }
@@ -13,4 +13,12 @@ RSpec.describe "The students datatable", js: true do
   subject { page }
 
   it { should have_content classroom.name }
+
+  it "displays all the students' emails" do
+    within "#students-table" do
+      students.each do |student|
+        expect(page).to have_content student.email
+      end
+    end
+  end
 end
