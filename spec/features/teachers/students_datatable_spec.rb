da14bfac12a34c43b8b7f489a38c3e95034da5cc
Sam Lau <samlau95@gmail.com>
2015-01-24 20:14:42 -0800

    Fix hound dot position

--- spec/features/teachers/students_datatable_spec.rb
+++ spec/features/teachers/students_datatable_spec.rb
@@ -21,4 +21,36 @@ RSpec.describe "The students datatable" do
       end
     end
   end
+
+  describe "action buttons" do
+    it "only shows delete for confirmed student" do
+      within id_of confirmed_student do
+        expect(page).to have_css "a.delete-icon"
+        expect(page).not_to have_css "a.email-icon"
+      end
+    end
+
+    it "shows delete and reconfirm for unconfirmed student" do
+      within id_of unconfirmed_student do
+        expect(page).to have_css "a.delete-icon"
+        expect(page).to have_css "a.email-icon"
+      end
+    end
+
+    describe "reconfirm button", uses_jobs: true do
+      it "sends an email" do
+        within id_of unconfirmed_student do
+          expect { find("a.email-icon").click }.to change { ActionMailer::Base.deliveries.count }.by 1
+        end
+      end
+    end
+
+    describe "delete button", js: true do
+      it "deletes the student" do
+        within id_of unconfirmed_student do
+          expect { find("a.delete-icon").click }.to change { Student.count }.by(-1)
+        end
+      end
+    end
+  end
 end
