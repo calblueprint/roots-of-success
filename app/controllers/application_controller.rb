fdd91182de30b027edcb37671b6232ba5343cd9d
Sam Lau <sam@khanacademy.org>
2015-06-10 00:38:32 -0700

    Fix CORS origin headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,26 @@ class ApplicationController < ActionController::Base
   include AuthorizationHelpers
 
   before_action :set_header
+  before_action :cors_preflight_check
+  after_action :cors_set_access_control_headers
 
   def set_header
     @header = "no_header"
+  end
+
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      headers['Access-Control-Max-Age'] = '1728000'
+    end
+  end
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
   end
 end
