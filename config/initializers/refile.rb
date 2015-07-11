1838fb4560b551be786abea3d513fcd7550d700a
Sam Lau <samlau95@gmail.com>
2015-07-10 19:39:57 -0700

    Fix s3 bucket prefix and countfront distribution

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -4,12 +4,12 @@ unless Rails.env.test?
   aws_config = {
     access_key_id: ENV["AWS_ACCESS_KEY_ID"],
     secret_access_key: ENV["AWS_SECRET_KEY"],
-    region: "us-west-1",
+    region: "us-west-2",
     bucket: "roots-#{Rails.env}"
   }
 
   Refile.cache = Refile::S3.new(max_size: 500.kilobytes, prefix: "cache", **aws_config)
   Refile.store = Refile::S3.new(prefix: "store", **aws_config)
 
-  Refile.host = "//da1avfhquctvk.cloudfront.net"
+  Refile.host = "//da1avfhquctvk.cloudfront.net" unless Rails.env.development?
 end
