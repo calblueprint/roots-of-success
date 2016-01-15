cf8b6006126bac4d3d32ed1e1bb982467f964cec
Sam Lau <samlau95@gmail.com>
2016-01-15 13:23:22 -0800

    Add latest changelog item to teacher dashboard
    Summary:
    Not particularly fond of the way it looks, but it'll do for now.
    
    Test Plan:
    Visit http://rails.me:3000/teachers/dashboard and verify that the
    latest changelog item shows up.
    
    Auditors: sam
    
    Projects: #roots-of-success

--- app/controllers/teachers/dashboard_controller.rb
+++ app/controllers/teachers/dashboard_controller.rb
@@ -4,9 +4,15 @@ module Teachers
 
     def show
       @teacher = current_teacher
-      @classrooms = ClassroomDecorator.decorate_collection(
-        @teacher.classrooms.active.includes(:students).includes(:presented_learning_modules)
-      )
+      # This query will eventually get really slow because we essentially do a
+      # Student.where for each active classroom.
+      #
+      # TODO(sam): Make it faster.
+      @classrooms = @teacher.classrooms.active
+                    .includes(:students)
+                    .includes(:presented_learning_modules)
+                    .decorate
+      @latest_changelog_item = ChangelogItem.first.decorate
     end
   end
 end
