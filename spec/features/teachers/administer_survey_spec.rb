214e564afc30f336e788b1a7d47b735f9565d8a6
Sam Lau <samlau95@gmail.com>
2016-05-28 00:49:53 -0700

    Fix specs

--- spec/features/teachers/administer_survey_spec.rb
+++ spec/features/teachers/administer_survey_spec.rb
@@ -3,9 +3,10 @@ require "rails_helper"
 RSpec.describe "The administer surveys page" do
   before { ParticipantSurvey.destroy_all }
   let(:teacher) { create :teacher }
-  let!(:participant_survey) { create :participant_survey }
-  let!(:teacher_surveys) { create_pair :teacher_survey }
   let!(:classroom) { create :classroom, teacher: teacher }
+  let!(:participant_survey) { create :participant_survey, program: classroom.program }
+  let!(:not_in_program) { create :participant_survey }
+  let!(:teacher_survey) { create :teacher_survey, program: classroom.program }
 
   subject { page }
 
@@ -15,19 +16,15 @@ RSpec.describe "The administer surveys page" do
   end
 
   it { should have_content participant_survey.name }
-
-  it "shows list of teacher surveys" do
-    teacher_surveys.each do |survey|
-      expect(page).to have_content survey.name
-    end
-  end
+  it { should have_content teacher_survey.name }
+  it { should_not have_content not_in_program.name }
 
   describe "participant survey link" do
     it "calls AdministerSurvey service object" do
       expect(AdministerSurveyToClassroom).to(
         receive(:execute).with(classroom: classroom, survey: participant_survey).and_return(true)
       )
-      find("#participant-survey").click
+      find("##{dom_id participant_survey}").click
     end
   end
 end
