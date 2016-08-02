568335e0f96d0b3e70d7d7a5c44009b718e68da7
michelleni <michelleni@berkeley.edu>
2016-08-02 04:44:22 -0700

    draft howto guides: dropdown menu

--- app/controllers/teachers/guide_controller.rb
+++
@@ -1,7 +0,0 @@
-module Teachers
-  class GuideController < BaseController
-    def index
-      @quide_embed_code = '<iframe src="https://docs.google.com/document/d/11sP1Y1KM6Xh5vmjsuJSmvfveT6MZORlwxNG2ErTMwmw/pub?embedded=true"></iframe>'
-    end
-  end
-end
