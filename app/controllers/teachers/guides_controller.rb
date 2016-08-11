0dec3cb36c916c600d4552d09e7c22712d524818
michelleni <michelleni@berkeley.edu>
2016-08-10 17:19:32 -0700

    Guide embed codes moved

--- app/controllers/teachers/guides_controller.rb
+++ app/controllers/teachers/guides_controller.rb
@@ -1,13 +1,9 @@
 module Teachers
   class GuidesController < BaseController
     def getting_started
-      @guide_embed_code = '<iframe src="https://docs.google.com/document/d/11sP1Y1KM6Xh5vmjsuJSmvfveT6MZORlwxNG2ErTMwmw/pub?embedded=true
-      "></iframe>'
     end
 
     def surveys_and_quizzes
-      @guide_embed_code = '<iframe src="https://docs.google.com/document/d/1oKygiwON0C87Tl1BGNHAZ6hSsQIZndNU4sjxOGE_djY/pub?embedded=true
-      "></iframe>'
     end
   end
 end
