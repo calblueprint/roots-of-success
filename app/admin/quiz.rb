ba7d042efd5332600a6cfd43ca1104b206796626
michelleni <michelleni@berkeley.edu>
2016-09-21 23:59:45 -0700

    Add quiz model to admin

---
+++ app/admin/quiz.rb
@@ -0,0 +1,23 @@
+ActiveAdmin.register Quiz do
+ permit_params :name, :form_embed_code, :program_id
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :program
+    actions
+  end
+
+  filter :name
+  filter :program
+
+  form do |f|
+    f.inputs "Quiz Details" do
+      f.input :name
+      f.input :program
+      f.input :form_embed_code
+    end
+    f.actions
+  end
+end
